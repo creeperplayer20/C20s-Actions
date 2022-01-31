36648fc4f10a4c44cf1dad740c10171f223c5789
creeperplayer20 <42560781+creeperplayer20@users.noreply.github.com>
2022-01-31 20:20:10 +0100

    Create Main.php

---
+++ src/Main.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace creeperplayer20\actions;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+
+class Main extends PluginBase implements Listener {
+
+public function onLoad() : void {
+
+$this->getLogger()->info("[§aC20§r - §eActions§r] §6 Plugin is loading!");
+
+}
+public function onEnable() : void{
+
+$this->getLogger()->info("[§aC20§r - §eActions§r] §6 Plugin is loaded!");
+$this->getServer()->getPluginManager()->registerEvents($this, $this);
+$this->reloadConfig();
+$this->saveDefaultConfig();
+$this->getConfig()->save();
+
+}
+
+public function onDisable() : void{
+
+$this->getLogger()->info("[§aC20§r - §eActions§r] §4 Plugin is disabled!");
+
+}
+
+public function onJoin(PlayerJoinEvent $event){
+
+$player = $event->getPlayer();
+$name = $player->getName();
+$event->setJoinMessage(str_replace("\$name", $name, $this->getConfig()->get("join")));;
+
+}
+
+public function onQuit(PlayerQuitEvent $event){
+
+$player = $event->getPlayer();
+$name = $player->getName();
+$event->setQuitMessage(str_replace("\$name", $name, $this->getConfig()->get("leave")));
+
+}
+}
